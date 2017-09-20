c22a847157cb46bfb5470ff5235813d0779d6fda
dlapcenko <dlapcenko@salmon.com>
2017-09-20 16:15:43 +0100

    CRA runs as a client correctly low

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = app => {
-	console.log(__dirname);
 	app.get("/", (req, res) => {
   		res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 	});
